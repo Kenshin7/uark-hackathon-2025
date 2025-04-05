f26916dd8d1eaf55b028068e22c369667437bb14
Kenshin7 <rithyka9@gmail.com>
2025-04-05 14:23:37 -0400

    update

--- components/WeightTracker.js
+++ components/WeightTracker.js
@@ -1,35 +1,53 @@
 import { useState } from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import ProgressBar from "./weight-tracker/ProgressBar";
 import WeightInput from "./weight-tracker/WeightInput";
 import GoalInput from "./weight-tracker/GoalInput";
-import WeightBar from "./weight-tracker/ProgressBar";
-import WeightStats from "./weight-tracker/WeightStats";
+
+const defaultWeight = 200;
 
 export default function WeightTracker() {
-	const [weightHistory, setWeightHistory] = useState([]);
+	const [weightHistory, setWeightHistory] = useState([defaultWeight]);
 	const [goal, setGoal] = useState([]);
 
 	const pushFunction = (weight) => {
 		setWeightHistory(weightHistory.concat([weight]));
 	};
 
+	const setFunction = (goal) => {
+		setGoal(goal);
+		setWeightHistory([weightHistory[weightHistory.length - 1]]);
+	};
+
 	return (
-		<>
-			<WeightInput
-				visible={true}
-				pushFunction={pushFunction}/>
-			
-			<GoalInput
-				visible={true}
-				setFunction={setGoal}/>
+		<View
+			style={styles.main}>
+			<ProgressBar
+				weightHistory={weightHistory}
+				goal={goal}/>
 
-			<WeightBar
-				weightHistory = {weightHistory[0]}
-				goal = {goal[0]}/>
+			<View
+				style={styles.bottomHalf}>
+				<GoalInput
+					setFunction={setFunction}/>
 
-			<WeightStats
-				weightHistory={weightHistory}/>
-		</>
+				<WeightInput
+					pushFunction={pushFunction}/>
+			</View>
+		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	main: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "space-around"
+	},
+	bottomHalf: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-evenly"
+	}
+});
 
--- components/weight-tracker/WeightInput.js
+++ components/weight-tracker/WeightInput.js
@@ -2,29 +2,26 @@
 //https://callstack.github.io/react-native-paper/docs/components/TextInput/
 
 import { useState } from "react";
-import { Button, Modal, TextInput } from "react-native-paper";
+import { Platform, View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
 import { validate } from "../../utils/Utilities.js";
 
 export default function WeightInput(props) {
-	const [visible, setVisible] = useState(props.visible);
 	const [weightInput, setWeightInput] = useState("");
 	
 	return (
-		<Modal
-			visible={visible}
-			onDismiss={() => { setVisible(false); }}>
-
+		<View>
 			<TextInput
 				label="Today's Weight (lbs.)"
 				value={weightInput}
-				onChangeText={(t) => { setWeightInput(validate(t.toString())); }}/>
+				onChangeText={(t) => { setWeightInput(validate(t.toString())); }}
+				keyboardType={(Platform.OS == "android") ? "numeric" : "number-pad"}/>
 
 			<Button
-				onPress={() => { props.pushFunction(Number(weightInput)); setVisible(false); }}>
-				Okay
+				onPress={() => { props.pushFunction(Number(weightInput)); }}>
+				Record Weight
 			</Button>
-
-		</Modal>
+		</View>
 	);
 };
 
